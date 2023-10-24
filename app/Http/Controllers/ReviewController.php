fa635c8ea86325c95821c12fe9d915ccc558ec48
unknown <zachariaachref.salem@esprit.tn>
2023-10-24 13:14:25 +0200

    final review with chart

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Models\Review;
+use App\Models\Tag;
+use App\Models\Product;
+
+
 use Illuminate\Http\Request;
 
 class ReviewController extends Controller
@@ -13,35 +16,94 @@ class ReviewController extends Controller
         $reviews = Review::all();
         return view('admin.Review.index', compact('reviews'));
     }
-    public function show($id){
-        $review= Review::findorFail($id);
+    public function show($id)
+    {
+        $review = Review::findorFail($id);
         return view('admin.Review.show', compact('review'));
-
     }
     public function create()
-{
-    return view('admin.Review.create');
-}
-    public function store(Request $request){
-        $review =Review::create($request->all());
-        return redirect()->route('reviews.index')->with('success', 'Review created successfully');
+    {
+        $tags = Tag::all(); // Assuming you have a "Tag" model for tags
+        return view('admin.Review.create', compact('tags'));
     }
-    public function edit($id)
-{
-    $review = Review::findOrFail($id);
-    return view('admin.Review.edit', compact('review'));
-}
-public function update(Request $request, $id)
-{
-    $review = Review::findOrFail($id);
-    $review->update($request->all());
-    return redirect()->route('reviews.index')->with('success', 'Review updated successfully');
-}
+    public function test()
+    {
+        return view('admin.Review.review');
+    }
+    public function store(Request $request)
+    {
+        $review = new Review($request->except('tags'));
 
-    public function delete(Request $request, $id){
+        // Save the review
+        $review->save();
+
+        // Attach the selected tags to the review
+        $review->tags()->attach($request->input('tags'));
+
+        return redirect()->route('produits')->with('success', 'Review created successfully');
+    }
+
+    public function edit($id)
+    {
+        $review = Review::findOrFail($id);
+
+        $tags = Tag::all();
+        return view('FrontClient.editReviewForm', compact('review', 'tags'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $review = Review::findOrFail($id);
+        $review->note = $request->input('note');
+        $review->update($request->all());
+
+        return response()->json(['message' => 'Review updated successfully']);
+    }
+
+
+
+    public function delete(Request $request, $id)
+    {
         $review = Review::findOrFail($id);
         $review->delete();
         return redirect()->route('reviews.index')->with('success', 'Review Deleted successfully');
     }
+    public function tagsChart()
+    {
+        $tags = Tag::all();
+        $tagCounts = [];
 
+        foreach ($tags as $tag) {
+            $tagCounts[] = $tag->reviews()->count();
+        }
+
+        return view('admin.dashboard.tags_chart', compact('tags', 'tagCounts'));
+    }
+    public function combinedCharts()
+    {
+        $tags = Tag::all();
+        $tagCounts = [];
+
+        foreach ($tags as $tag) {
+            $tagCounts[] = $tag->reviews()->count();
+        }
+
+        $products = Product::with(['review' => function ($query) {
+            $query->select('product_id', \DB::raw('AVG(note) as average_note'))
+                ->whereBetween('note', [0, 5])
+                ->groupBy('product_id');
+        }])->get();
+
+        return view('admin.dashboard.charts', compact('tags', 'tagCounts', 'products'));
+    }
+    public function averageNoteChart()
+    {
+        $products = Product::with(['review' => function ($query) {
+            $query->select('product_id', \DB::raw('AVG(note) as average_note'))
+                ->whereBetween('note', [0, 5])
+                ->groupBy('product_id');
+        }])->get();
+
+        return view('admin.dashboard.average_note_chart', compact('products'));
+    }
 }
